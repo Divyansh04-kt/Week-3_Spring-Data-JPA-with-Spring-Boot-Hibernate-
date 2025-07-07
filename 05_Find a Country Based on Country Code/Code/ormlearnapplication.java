b3b9154a454b0c1e4ab0a2d1ff4c4e3e0c761cf9
Divyansh Katakwar <divyanshkatakwar8@gmail.com>
2025-07-07 16:25:24 +0530

    Create ormlearnapplication.java

---
+++ 05_Find a Country Based on Country Code/Code/ormlearnapplication.java
@@ -0,0 +1,39 @@
+package com.cognizant.orm_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.service.CountryService;
+import com.cognizant.spring_learn.service.exception.CountryNotFoundException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    public static CountryService countryService;
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+        countryService = context.getBean(CountryService.class);
+
+        getCountryTest();  
+    }
+
+    private static void getCountryTest() {
+        LOGGER.info("Start");
+        try {
+            Country country = countryService.findCountryByCode("IN");
+            LOGGER.debug("Country: {}", country);
+        } catch (CountryNotFoundException e) {
+            LOGGER.error("Exception: {}", e.getMessage());
+        }
+        LOGGER.info("End");
+    }
+}
