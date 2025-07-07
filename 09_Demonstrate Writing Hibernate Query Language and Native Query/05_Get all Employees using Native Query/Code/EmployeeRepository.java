1072813b6833a5eec8dc89d9a164db5e732c9245
Divyansh Katakwar <divyanshkatakwar8@gmail.com>
2025-07-07 20:42:14 +0530

    Update and rename m to EmployeeRepository.java

---
+++ 09_Demonstrate Writing Hibernate Query Language and Native Query/05_Get all Employees using Native Query/Code/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.cognizant.orm_learn.repository;
+
+import com.cognizant.orm_learn.model.Employee;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    @Query(value = "SELECT * FROM employee", nativeQuery = true)
+    List<Employee> getAllEmployeesNative();
+}
