112c4d80fe73ed2eef23e8a4598645ae036df38e
Divyansh Katakwar <divyanshkatakwar8@gmail.com>
2025-07-07 21:48:19 +0530

    Create EmployeeService.java

---
+++ 09_Demonstrate Writing Hibernate Query Language and Native Query/05_Get all Employees using Native Query/Code/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.cognizant.orm_learn.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.cognizant.orm_learn.model.Employee;
+import com.cognizant.orm_learn.repository.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployeesNative() {
+        return employeeRepository.getAllEmployeesNative();
+    }
+}
